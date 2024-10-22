c6f4c95d868aca7a451f6b78dfd734cc11222b23
chary <smkzachatac@gmail.com>
2024-10-21 20:11:22 -0700

    Vision

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "IJBB";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "689c4d6fa2e24bf532fc50ad2f2dc88b14601e2e";
+  public static final String GIT_DATE = "2024-10-14 18:24:09 PDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-10-14 20:20:37 PDT";
+  public static final long BUILD_UNIX_TIME = 1728962437930L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
